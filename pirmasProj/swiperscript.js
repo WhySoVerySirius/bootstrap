aa0b1c4c45ba7a27ca074db52e7c2e66c270073c
WhySoVerySirius <vytartast@gmail.com>
2022-01-10 18:00:48 +0200

    meh2

--- pirmasProj/swiperscript.js
+++ pirmasProj/swiperscript.js
@@ -12,15 +12,4 @@ const swiper = new Swiper('.swiper', {
     pagination: {
         el: '.swiper-pagination',
     },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
 });
